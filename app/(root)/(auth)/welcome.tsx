3b9df52df94fef8c11cf33278e7cadcdf69d359f
Pheonix2852 <sagnikkumbhakar.2003.va@gmail.com>
2026-04-09 02:00:26 +0530

    feat: Implement authentication flow with welcome, sign up, and sign in screens
    - Added welcome screen with onboarding slides
    - Created sign up and sign in screens with input fields and buttons
    - Integrated navigation between authentication screens
    - Updated app.json to include splash screen configuration
    - Added custom button and input field components for better UI
    - Included new fonts and icons for enhanced design
    - Configured Tailwind CSS for styling
    - Added type definitions for images and various components

---
+++ app/(root)/(auth)/welcome.tsx
@@ -0,0 +1,69 @@
+import CustomButton from "@/components/CustomButton";
+import { onboarding } from "@/constants";
+import { Image } from "expo-image";
+import { router } from "expo-router";
+import React, { useRef, useState } from "react";
+import { Text, TouchableOpacity, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import Swiper from "react-native-swiper";
+
+const welcome = () => {
+  const swiperRef = useRef<Swiper>(null);
+  const [activeIndex, setActiveIndex] = useState(0);
+  const isLastSlide = activeIndex === onboarding.length - 1;
+
+  return (
+    <SafeAreaView className="flex h-full items-center justify-between bg-white">
+      <TouchableOpacity
+        onPress={() => {
+          router.replace("/(root)/(auth)/signUp");
+        }}
+        className="w-full flex justify-end items-end p-5"
+      >
+        <Text className="text-black text-md font-JakartaBold">Skip</Text>
+      </TouchableOpacity>
+
+      <Swiper
+        ref={swiperRef}
+        loop={false}
+        dot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#E2E8F0] rounded-full" />
+        }
+        activeDot={
+          <View className="w-[32px] h-[4px] mx-1 bg-[#0286FF] rounded-full" />
+        }
+        onIndexChanged={(index) => setActiveIndex(index)}
+      >
+        {onboarding.map((item) => (
+          <View key={item.id} className="flex justify-center items-center p-5">
+            <Image
+              source={item.image}
+              style={{ width: "100%", height: 300 }}
+              contentFit="contain"
+            />
+            <View className="flex flex-row items-center justify-center w-full mt-10">
+              <Text className="text-black text-3xl font-bold mx-10 text-center">
+                {item.title}
+              </Text>
+            </View>
+            <Text className="text-lg font-JakartaSemiBold text-center text-[#858585] mx-10 mt-3">
+              {item.description}
+            </Text>
+          </View>
+        ))}
+      </Swiper>
+
+      <CustomButton
+        title={isLastSlide ? "Get Started" : "Next"}
+        onPress={() =>
+          isLastSlide
+            ? router.replace("/(root)/(auth)/signUp")
+            : swiperRef.current?.scrollBy(1)
+        }
+        className="w-11/12 mt-10"
+      />
+    </SafeAreaView>
+  );
+};
+
+export default welcome;
--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,6 @@
-import { Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Redirect } from "expo-router";
 import "../global.css";
 
 export default function Home() {
-  return (
-    <SafeAreaView className="flex-1 items-center justify-center bg-red">
-      <Text className="text-xl font-bold text-blue-500">
-        Welcome to Nativewind!
-      </Text>
-    </SafeAreaView>
-  );
+  return <Redirect href={"/(root)/(auth)/welcome"} />;
 }
